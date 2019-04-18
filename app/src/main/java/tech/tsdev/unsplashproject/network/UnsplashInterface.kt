a6212057c4766f3473526bdf2b6872dcdb20814d
TaeseongYun <yts0646@gmail.com>
2019-04-19 00:47:18 +0900

    23: .gitignore AccessKey Hide

--- app/src/main/java/tech/tsdev/unsplashproject/network/UnsplashInterface.kt
+++ app/src/main/java/tech/tsdev/unsplashproject/network/UnsplashInterface.kt
@@ -22,26 +22,26 @@ interface  UnsplashInterface {
 
     //Path값 주고 {Path} Get 넣어주면 매번 바뀐다
     //클릭 하였을 때 해당 유저 이름의 사진 불러오기 위한 쿼리
-    @GET("{username}?client_id=50fda0b295b432b006dc70b08f3ca128a32efa16f92cec67d97cb41d559d3ff1")
+    @GET("{username}?client_id=YOUR_ACCESS_KEY")
     fun getDetailInfo(
         @Path("username") username: String
     ): Call<SinglePhoto>
 
 
     //최근 사진 불러오게 하는 쿼리
-    @GET("?client_id=50fda0b295b432b006dc70b08f3ca128a32efa16f92cec67d97cb41d559d3ff1")
+    @GET("?client_id=YOUR_ACCESS_KEY")
     fun getUpsplashLatestPhotos(
         @Query("page") page: Int,
         @Query("per_page") per_page: Int
     ): Call<List<LatestPhotos>>
 
     //사진 다운로드 하게 하는 쿼리
-    @GET("{userId}/download?client_id=50fda0b295b432b006dc70b08f3ca128a32efa16f92cec67d97cb41d559d3ff1")
+    @GET("{userId}/download?client_id=YOUR_ACCESS_KEY")
     fun getDownloadImage(
         @Path("userId") userId: String
     ): Call<JSONObject>
 
-    @GET("photos/random?client_id=50fda0b295b432b006dc70b08f3ca128a32efa16f92cec67d97cb41d559d3ff1")
+    @GET("photos/random?client_id=YOUR_ACCESS_KEY")
     fun getRandomPhoto(
         @Query("count") count: Int
     ): Call<List<RandomPhoto>>
